a3ccc55b2f8c649184732862e493ec19f77b5d06
Alan K <akillian@outlook.com>
2021-03-24 13:29:09 -0400

    commit

--- src/components/henry_VIIIBio.vue
+++ src/components/henry_VIIIBio.vue
@@ -439,6 +439,69 @@ export default {
         item.pathEnd = navPath.getTotalLength();
       });
     } // end function drawPath()
+
+    function syncPath() {
+      const someElsAreVisible = () =>
+          nav.querySelectorAll(`.${visibleClass}`).length > 0,
+        thisElIsVisible = el => el.classList.contains(visibleClass),
+        pathLength = navPath.getTotalLength();
+
+      let pathStart = pathLength,
+        pathEnd = 0,
+        lastPathStart,
+        lastPathEnd;
+
+      navItems.forEach(item => {
+        if (thisElIsVisible(item.listItem)) {
+          pathStart = Math.min(item.pathStart, pathStart);
+          pathEnd = Math.max(item.pathEnd, pathEnd);
+        }
+      });
+
+      if (someElsAreVisible() && pathStart < pathEnd) {
+        if (pathStart !== lastPathStart || pathEnd !== lastPathEnd) {
+          const dashArray = `1 ${pathStart} ${pathEnd -
+            pathStart} ${pathLength}`;
+
+          navPath.style.setProperty("stroke-dashoffset", "1");
+          navPath.style.setProperty("stroke-dasharray", dashArray);
+          navPath.style.setProperty("opacity", 1);
+        }
+      } else {
+        navPath.style.setProperty("opacity", 0);
+      }
+
+      lastPathStart = pathStart;
+      lastPathEnd = pathEnd;
+    } // end the syncpath() Function
+
+    function markVisibleSection(observedEls) {
+      observedEls.forEach(observedEl => {
+        const id = observedEl.target.getAttribute("id"),
+          anchor = document.querySelector(`nav li a[href="#${id}"]`);
+
+        if (!anchor) {
+          return false;
+        }
+
+        const listItem = anchor.parentElement;
+
+        if (observedEl.isIntersecting) {
+          listItem.classList.add(visibleClass);
+        } else {
+          listItem.classList.remove(visibleClass);
+        }
+
+        syncPath();
+      });
+    } // end markVisibleSection() Function
+
+    // =============== Draw Path and observe ========== //
+
+    drawPath();
+
+    const observer = new IntersectionObserver(markVisibleSection);
+    elementsToObserve.forEach(thisEl => observer.observe(thisEl));
   }
 };
 </script>
