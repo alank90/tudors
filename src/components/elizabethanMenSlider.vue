246f89468b681abfc906f505ae82f81291e4acfa
Alan Killian <akillian@outlook.com>
2021-04-24 17:25:27 -0400

    commit

--- src/components/elizabethanMenSlider.vue
+++ src/components/elizabethanMenSlider.vue
@@ -1,34 +1,71 @@
 <template>
   <div class="slider-container">
-    <img
-      src="../assets/img/elizabethsMen/earlofEssex.jpg"
-      alt="The Earl of Essex"
-    />
+    <div class="image-container">
+      <img
+        src="../assets/img/elizabethsMen/earlofEssex.jpg"
+        alt="The Earl of Essex"
+      />
 
-    <img
-      src="../assets/img/elizabethsMen/francis_bacon.jpg"
-      alt="The Earl of Essex"
-    />
-    <img
-      src="../assets/img/elizabethsMen/chris_hatton.jpg"
-      alt="Chris Hatton"
-    />
+      <img
+        src="../assets/img/elizabethsMen/francis_bacon.jpg"
+        alt="The Earl of Essex"
+      />
+      <img
+        src="../assets/img/elizabethsMen/chris_hatton.jpg"
+        alt="Chris Hatton"
+      />
 
-    <img
-      src="../assets/img/elizabethsMen/will_shakespeare.jpg"
-      alt="William Shakespeare"
-    />
+      <img
+        src="../assets/img/elizabethsMen/will_shakespeare.jpg"
+        alt="William Shakespeare"
+      />
+    </div>
+
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <path stroke-width="6" />
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-  name: "slider"
+  name: "slider",
+  mounted() {
+    // ===== Vars ========= //
+    const imageItems = document.querySelectorAll("img"),
+      sliderPath = document.querySelector("svg path");
+
+    // ======== Functions ==================== //
+    function drawPath() {
+      let path = [];
+
+      imageItems.forEach((item, i) => {
+        if (i === 0) {
+          const imagePosition1 = item.getBoundingClientRect();
+          const imagePosition2 = imageItems[i + 1].getBoundingClientRect();
+          console.log({ imagePosition2 });
+          // Right edge of current image
+          const x1 = imagePosition1.right;
+          const y = imagePosition1.bottom + imagePosition1.height / 2;
+          // Left edge of next image
+          const x2 = imagePosition2.left;
+
+          path.push("M", x1, y, "L", x2, y);
+          item.pathStart = 0;
+        }
+
+        sliderPath.setAttribute("d", path.join(" "));
+      });
+    }
+
+    // ======== Draw Slider Line ================== //
+    drawPath();
+  }
 };
 </script>
 
 <style scoped>
-.slider-container {
+.image-container {
   display: flex;
   flex-wrap: nowrap;
   width: 100%;
@@ -36,7 +73,7 @@ export default {
   align-items: flex-start;
 }
 
-.slider-container > img {
+.image-container > img {
   width: 100px;
   height: auto;
 }
