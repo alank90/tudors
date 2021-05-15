ceee68bab9ce5893943c9a59e3992cd5dbbeb7ea
Alan Killian <akillian@outlook.com>
2021-05-14 21:31:15 -0400

    commit.

--- src/views/assets/navBarScroll.js
+++ src/views/assets/navBarScroll.js
@@ -1,19 +1,35 @@
 const navBarScroll = () => {
   // ============== Vars ========================== //
-  const elementsToObserve = document.querySelectorAll(
-    "section[id], nav:not(.main-head-nav) > ul > li"
-  );
+  const elementsToObserve = document.querySelectorAll("section[id]");
 
   //
   function scrollNavBar(observedEls) {
+    const navBar = document.querySelector(".edward-bio");
+    const pathEl = document.querySelector("path");
+
     observedEls.forEach(observedEl => {
       if (observedEl.isIntersecting) {
         // Filter out the nav observed elements //
         // Observed element gets selected when a <section> comes into view //
         if (observedEl.target.nodeName === "SECTION") {
           const idStr = observedEl.target.id;
+          const navStr = document.querySelector(`[data-name=${idStr}]`);
+          console.log("NavStr is ", navStr);
+
+          const navElementTop = navStr.getBoundingClientRect();
+          const pageHeight = window.innerHeight;
+          console.log("Element Position: ", navElementTop);
+          console.log("Page height: ", pageHeight);
           console.log("The observed id is: ", idStr);
           console.log("Element in view is: ", observedEl.target);
+
+          // If navElementTop.top > pageHeight the navElement isnt visible //
+          // We then translateY the navBar up
+          /* if (navElementTop.top > pageHeight) {
+            navBar.style.transform = "translateY(-200px)";
+            console.log("Adjusting navBar.");
+            pathEl.style.transform = "translateY(-200px)";
+          } */
         }
       }
     });
