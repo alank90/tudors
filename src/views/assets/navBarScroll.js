39d58368119440b6f020ffb0c06ebedd0c82ba45
Alan K <akillian@outlook.com>
2021-05-24 14:26:24 -0400

    commit

--- src/views/assets/navBarScroll.js
+++ src/views/assets/navBarScroll.js
@@ -1,18 +1,16 @@
 const navBarScroll = () => {
   // ============== Vars ========================== //
   const elementsToObserve = document.querySelectorAll("section[id]");
+  const navBar = document.querySelector(".edward-bio");
+  const navBarTop = navBar.getBoundingClientRect().top;
 
   //
   function scrollNavBar(observedEls) {
-    const navBar = document.querySelector(".edward-bio");
-    /* const scrollPageHeight = window.innerHeight + window.scrollY; //Page height w/scrolling
-    console.log("scrollPage Height", scrollPageHeight); */
+    console.log("navBarTop position is : ", navBarTop);
     const viewportHeight = window.innerHeight;
 
     observedEls.forEach(observedEl => {
-      let shiftAmt = "";
       if (observedEl.isIntersecting) {
-        console.log("New Element to observe: ");
         console.log("viewport height: ", viewportHeight);
         // idStr is the <section> that has come into view //
         const idStr = observedEl.target.id;
@@ -21,11 +19,14 @@ const navBarScroll = () => {
         console.log("NavStr is ", navStr);
         const navElPosition = navStr.getBoundingClientRect();
         const sectionElPosition = observedEl.target.getBoundingClientRect();
-        console.log("Navbar Element Top Position: ", navElPosition);
-        console.log("The <section> element in view is: ", observedEl.target);
-        console.log("<section> element position is: ", sectionElPosition);
 
         const navElTop = navElPosition.top;
+        let shiftAmt = navElTop;
+
+        const sectionElTop = sectionElPosition.top;
+        const sectionElBottom = sectionElPosition.bottom;
+        console.log("Current Navbar Element Top Position: ", navElTop);
+        console.log("The <section> element in view is: ", observedEl.target);
         console.log(
           "The top of the currently observed <section> entry is: ",
           sectionElPosition.top
@@ -36,8 +37,9 @@ const navBarScroll = () => {
         );
 
         // Check if the newly entered <article> section's corresponding navbar entry
-        // is out of viewport's range
-        if (navElTop > viewportHeight) {
+        // entered the viewport from the top
+        if (sectionElTop < sectionElBottom && sectionElTop > 0) {
+          console.log("Scrolling Down!!!");
           shiftAmt = `${navElTop + 15}px`;
           // Move the navBar up via the translateY() transform
           navBar.style.transform = `translateY(-${shiftAmt})`;
@@ -48,7 +50,7 @@ const navBarScroll = () => {
         ) {
           shiftAmt = `${navElTop + 15}px`;
           // Move the navBar down via the translateY() transform
-          navBar.style.transform = `translateY(${shiftAmt})`;
+          /* navBar.style.transform = `translateY(${shiftAmt})`; */
           console.log("nav element is down shift Amt : ", shiftAmt);
         }
       }
