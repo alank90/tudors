61b25ba52f1b6ab6a20c26892ce20bf5389ce720
Alan K <akillian@outlook.com>
2021-05-26 14:26:01 -0400

    commit

--- src/views/assets/navBarScroll.js
+++ src/views/assets/navBarScroll.js
@@ -2,32 +2,26 @@ const navBarScroll = () => {
   // ============== Vars ========================== //
   const elementsToObserve = document.querySelectorAll("section[id]");
   const navBar = document.querySelector(".edward-bio");
-  const navBarTop = navBar.getBoundingClientRect().top;
+
   let totalShiftAmount = 0;
 
   function scrollNavBar(observedEls) {
-    console.log("navBarTop position is : ", navBarTop);
     const viewportHeight = window.innerHeight;
 
     observedEls.forEach(observedEl => {
       if (observedEl.isIntersecting) {
-        console.log("viewport height: ", viewportHeight);
+        // ==== Vars ================= //
         // idStr is the <section> that has come into view //
         const idStr = observedEl.target.id;
         // navStr is the corresponding nav entry
         const navStr = document.querySelector(`[data-name=${idStr}]`);
-        console.log("NavStr is ", navStr);
+        console.log("Current NavStr is ", navStr);
         const navElDimensions = navStr.getBoundingClientRect();
-        console.log("navElDimensions are: ", navElDimensions);
         const sectionElPosition = observedEl.target.getBoundingClientRect();
-
         const navElTop = navElDimensions.top;
-
-        let shiftAmt = navElTop;
-
         const sectionElTop = sectionElPosition.top;
         const sectionElBottom = sectionElPosition.bottom;
-        console.log("Current Navbar Element Top Position: ", navElTop);
+
         console.log("The <section> element in view is: ", observedEl.target);
         console.log(
           "The top of the currently observed <section> entry is: ",
@@ -42,20 +36,33 @@ const navBarScroll = () => {
         // entered the viewport from the top
         if (sectionElTop < sectionElBottom && sectionElTop > 0) {
           console.log("Scrolling Down!!!");
+          // Check if observedEl.target corresponding navbar entry is
+          // visible. If not let's TranslateY() the navbar up
           if (navElTop > viewportHeight) {
             totalShiftAmount += -navElDimensions.height - 100;
             // Move the navBar up via the translateY() transform
             navBar.style.transform = `translateY(${totalShiftAmount}px)`;
-            console.log("nav element is up totalShiftAmt : ", totalShiftAmount);
+            console.log(
+              `Nav entry ${observedEl.target.id} is not in view. Let's shift navbar up ${totalShiftAmount}`
+            );
           }
         } else if (
           sectionElPosition.top < 0 &&
           navStr.dataset.name !== "intro"
         ) {
-          shiftAmt = `${navElTop + 15}px`;
+          console.log("Srolling up!!!");
+
+          // Check if observedEl.target corresponding navbar entry is
+          // visible. If not let's TranslateY() the navbar down
+          console.log(
+            `Lets check if current navElement ${navElTop} is in view ${viewportHeight}`
+          );
           // Move the navBar down via the translateY() transform
-          /* navBar.style.transform = `translateY(${shiftAmt})`; */
-          console.log("nav element is down shift Amt : ", shiftAmt);
+          // totalShiftAmount += navElDimensions.height + 100;
+          console.log(
+            `Nav entry ${observedEl.target.id} is not in view. Let's shift navbar up ${totalShiftAmount}`
+          );
+          /* navBar.style.transform = `translateY(${totalShiftAmount})`; */
         }
       }
     });
